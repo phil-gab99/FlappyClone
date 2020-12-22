c7d32ad12e98ea008200e1c4dd47cfe03f13e5e7
phil-gab99 <pgabriel999@hotmail.com>
2020-12-22 16:12:56 -0500

    Initial structure set

--- app/src/main/java/personal/philgab99_souleiman2/flappyclone/MainActivity.java
+++ app/src/main/java/personal/philgab99_souleiman2/flappyclone/MainActivity.java
@@ -1,7 +1,15 @@
 package personal.philgab99_souleiman2.flappyclone;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+* The class
+*
+* @author Souleiman Ayoub
+* @author Philippe Gabriel
+* @version 0.1 2020-12-22
+***/
 
 public class MainActivity extends AppCompatActivity {
 
